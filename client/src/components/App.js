8d698e4dace65abf2080b9def024bff376249e50
t8g <tmoyse@gmail.com>
2017-12-04 13:37:42 +0100

    feat: toggle reactive navbar menu

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,47 @@
-import './App.css'
+import './App.css';
 
-import { Link, Route, Switch } from 'react-router-dom'
-import React, { Component } from 'react'
+import { Link, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
 
-import Home from './Home'
-import UserForm from './UserForm'
-import Users from './Users'
+import Home from './Home';
+import UserForm from './UserForm';
+import Users from './Users';
+import classNames from 'classnames';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuActive: false,
+    };
+
+    this.toggleActive = this.toggleActive.bind(this);
+  }
+
+  toggleActive() {
+    this.setState({
+      menuActive: !this.state.menuActive
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-          <div className="navbar-menu">
+        <nav
+          className="navbar is-fixed-top is-primary"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="navbar-brand">
+            <button className={classNames({'button': true, 'navbar-burger': true, 'is-active': this.state.menuActive})} onClick={this.toggleActive}>
+              <span />
+              <span />
+              <span />
+            </button>
+          </div>
+
+          <div className={classNames({'navbar-menu': true, 'is-active': this.state.menuActive })} onClick={this.toggleActive}>
             <Link className="navbar-item" to="/">
               Home
             </Link>
@@ -31,8 +60,8 @@ class App extends Component {
           </div>
         </main>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;

fbfb59bede1b33a10589c42101db2af4edebc8ac
t8g <tmoyse@gmail.com>
2017-12-04 13:48:39 +0100

    toggle menu : après refacto (merci @delapouite et @naholyr)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,17 +9,12 @@ import Users from './Users';
 import classNames from 'classnames';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      menuActive: false,
-    };
-
-    this.toggleActive = this.toggleActive.bind(this);
+  state = {
+    menuActive: false
   }
 
-  toggleActive() {
+  toggleActive = () => {
     this.setState({
       menuActive: !this.state.menuActive
     });
@@ -34,14 +29,14 @@ class App extends Component {
           aria-label="main navigation"
         >
           <div className="navbar-brand">
-            <button className={classNames({'button': true, 'navbar-burger': true, 'is-active': this.state.menuActive})} onClick={this.toggleActive}>
+            <button className={classNames('button', 'navbar-burger', {'is-active': this.state.menuActive})} onClick={this.toggleActive}>
               <span />
               <span />
               <span />
             </button>
           </div>
 
-          <div className={classNames({'navbar-menu': true, 'is-active': this.state.menuActive })} onClick={this.toggleActive}>
+          <div className={classNames('navbar-menu', {'is-active': this.state.menuActive})} onClick={this.toggleActive}>
             <Link className="navbar-item" to="/">
               Home
             </Link>

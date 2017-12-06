b0be6d319c345ee5b0ff83d783c78aea1af5a688
Delapouite <delapouite@gmail.com>
2017-12-06 13:12:16 +0100

    feat(client): add Resources dummy menu

--- client/src/components/Resources.js
+++ client/src/components/Resources.js
@@ -28,7 +28,48 @@ class Resources extends Component<Props> {
         <h1 className="title">
           <T id="resources" />
         </h1>
-        {loading ? <Spinner /> : <div />}
+        <div className="columns">
+          <div className="column is-one-quarter">
+            <aside className="menu">
+              <p className="menu-label">Type</p>
+              <ul className="menu-list">
+                <li>
+                  <a>
+                    <span className="icon is-medium">
+                      <i className="fa fa-map" />
+                    </span>
+                    <T id="maps" />
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <span className="icon is-medium">
+                      <i className="fa fa-camera-retro" />
+                    </span>
+                    <T id="photos" />
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <span className="icon is-medium">
+                      <i className="fa fa-film" />
+                    </span>
+                    <T id="videos" />
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <span className="icon is-medium">
+                      <i className="fa fa-microphone" />
+                    </span>
+                    <T id="sounds" />
+                  </a>
+                </li>
+              </ul>
+            </aside>
+          </div>
+          <div className="column">{loading ? <Spinner /> : <div />}</div>
+        </div>
       </div>
     )
   }

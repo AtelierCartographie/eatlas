5651245c17188da4f5575795be71ed6475ba8a01
Nicolas Chambrier <naholyr@gmail.com>
2017-12-06 17:54:32 +0100

    feat(server): more details in error message sent back to user when doc failed to parse (status 400 then)

--- server/lib/routes/resources.js
+++ server/lib/routes/resources.js
@@ -32,8 +32,16 @@ exports.addFromGoogle = (req, res) => {
     .then(resource => resources.create(resource))
     // Respond only with resource's id
     .then(resource => res.send({ id: resource.id }))
-    // TODO more info about parsing errors
-    .catch(err => res.boom.badImplementation(err))
+    .catch(err => {
+      if (err.isJoi) {
+        return res.boom.badRequest(err.message, {
+          details: err.details,
+          object: err._object,
+          annotated: err.annotate(),
+        })
+      }
+      res.boom.badImplementation(err)
+    })
 }
 exports.addFromGoogle.schema = schemas.uploadFromGoogleDrive
 

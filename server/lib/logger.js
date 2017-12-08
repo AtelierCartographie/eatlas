d45dc3ce510333d058b6a3904e3750efd4e25a47
Nicolas Chambrier <naholyr@gmail.com>
2017-12-08 14:22:57 +0100

    feat(server): bunyan configuration

--- server/lib/logger.js
+++ server/lib/logger.js
@@ -1,3 +1,23 @@
-const bunyan = require('bunyan')
+'use strict'
 
-module.exports = bunyan.createLogger({ name: 'eatlas' })
+const bunyan = require('bunyan')
+const config = require('config')
+
+const defaults = {
+  streams: [
+    // TODO configure streams from 'config' object
+    {
+      level: 'info',
+      stream: process.stdout,
+    },
+    {
+      level: 'fatal',
+      stream: process.stderr,
+    },
+  ],
+  name: 'eatlas',
+  level: 'info', // trace|debug|info|warn|error|fatal
+  src: false,
+}
+
+module.exports = bunyan.createLogger(Object.assign({}, defaults, config.logger))

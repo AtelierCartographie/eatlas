ab836ab10588ca7aa0ac1123906bc26a3e52bd63
Delapouite <delapouite@gmail.com>
2018-01-15 11:07:53 +0100

    feat(docx): add argv support for parser cli

--- bin/parse-docx.js
+++ bin/parse-docx.js
@@ -3,8 +3,12 @@ const { resolve } = require('path')
 
 const { parseDocx } = require('../server/lib/doc-parser')
 
-const path = resolve(__dirname, '../docs/samples', '4A07 Paix négatives.docx')
+const path =
+  process.argv[2] ||
+  resolve(__dirname, '../docs/samples', '4A07 Paix négatives.docx')
 
-parseDocx(readFileSync(path))
-	.then(result => JSON.stringify(result, null, 2))
-	.then(console.log, console.error)
+const main = async path => {
+  const doc = await parseDocx(readFileSync(path))
+  return JSON.stringify(doc, null, 2)
+}
+main(path).then(console.log, console.error)

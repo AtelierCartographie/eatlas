6731c21d75ec9beac27932331168f0bca2a3957b
Delapouite <delapouite@gmail.com>
2018-01-18 16:29:44 +0100

    feat(ArticleForm): add ResourceField sub comp

--- client/src/components/ArticleForm.js
+++ client/src/components/ArticleForm.js
@@ -1,13 +1,60 @@
 // @flow
 
+import './ArticleForm.css'
+
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import Icon from './Icon'
+
+type RProps = {
+  node: Object,
+  resource: Object,
+}
+
+class _ResourceField extends Component<RProps> {
+  render() {
+    const { node, resource } = this.props
+    if (!resource)
+      return (
+        <div className="field">
+          <label className="label has-text-danger">
+            Resource not found <Icon icon="warning" />
+          </label>
+          <div className="control">
+            {node.id} {node.text}
+          </div>
+        </div>
+      )
+
+    let preview = null
+
+    // TODO dry with renderPreview in Resources
+    if (resource.type === 'image' && resource.images) {
+      // medium@1x is mandatory, we can count on it
+      const file = resource.images.medium['1x']
+      const url = (process.env.REACT_APP_PUBLIC_PATH_image || '/') + file
+      preview = <img className="preview" src={url} alt={file} />
+    }
+
+    return (
+      <div className="field">
+        <label className="label">Resource {resource.id}</label>
+        <div className="control">{preview}</div>
+      </div>
+    )
+  }
+}
+
+const ResourceField = connect(({ resources }, { node }) => {
+  return { resource: resources.list.find(r => r.id === node.id) }
+})(_ResourceField)
 
 type Props = {
-  article: any
+  article: any,
 }
 
 class ArticleForm extends Component<Props> {
-
   renderHeader(node, k) {
     return (
       <div className="field" key={k}>
@@ -55,14 +102,7 @@ class ArticleForm extends Component<Props> {
   }
 
   renderResource(node, k) {
-    return (
-      <div className="field" key={k}>
-        <label className="label">Resource</label>
-        <div className="control">
-          {node.id} {node.text}
-        </div>
-      </div>
-    )
+    return <ResourceField node={node} key={k} />
   }
 
   renderFootnotes(node, k) {

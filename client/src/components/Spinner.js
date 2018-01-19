b55bdb7b16d8e9ff570581503d67110d52dd24b1
Nicolas Chambrier <naholyr@gmail.com>
2018-01-19 01:03:10 +0100

    feat(spinner): uses font-awesome spinner + pulse for lighter and side-effect-free spinner

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
-
-// onClick to prevent form submission
-const noSubmit = e => e.preventDefault()
+import Icon from './Icon'
 
 class Spinner extends Component<{}> {
   render() {
     // bulma does not offer the is-loading class on span :(
-    return (
-      <button className="button is-loading is-rounded" onClick={noSubmit} />
-    )
+    return <Icon icon="spinner fa-pulse" />
   }
 }
 

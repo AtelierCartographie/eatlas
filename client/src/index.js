4cc04cc7233e9d789fe21758cafc76d57a4275df
Delapouite <delapouite@gmail.com>
2018-03-13 08:41:38 +0100

    feat(client): remove confusing stylesheet

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import './index.css'
 import Root from './components/Root'
 import store from './store'
 import { checkSession } from './api'

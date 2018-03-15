d57ad76a58666470dbf8891aa4e2a76567018429
Nicolas Chambrier <naholyr@gmail.com>
2018-03-15 12:48:47 +0100

    feat(slug): adds 'slugify' in universal-utils for URL generation (refs #77)

--- client/src/universal-utils.js
+++ client/src/universal-utils.js
@@ -82,3 +82,14 @@ exports.META_CONVERSION = {
   'article-associé': 'related-article',
   'Image header': 'image-header',
 }
+
+// https://gist.github.com/mathewbyrne/1280286
+exports.slugify = text =>
+  text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/[^\w-]+/g, '') // Remove all non-word chars
+    .replace(/--+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text

0f8582d7375f0aa2cf6e9a779383c40016fae4d3
Nicolas Chambrier <naholyr@gmail.com>
2018-03-16 13:12:36 +0100

    feat(search): WIP sorts results (refs #79)

--- server/lib/routes/search.js
+++ server/lib/routes/search.js
@@ -2,11 +2,20 @@
 
 const { resources: Resources } = require('../model')
 
+const sortVal = r => new Date(r.publishedAt)
+const sortDir = 'desc'
+
 exports.search = async (req, res) => {
   try {
     const resources = await Resources.list({
       query: { term: { status: 'published' } },
     })
+    resources.sort(
+      (r1, r2) =>
+        sortDir === 'desc'
+          ? sortVal(r2) - sortVal(r1)
+          : sortVal(r1) - sortVal(r2),
+    )
     res.send(resources)
   } catch (err) {
     res.boom.badImplementation(err)

2682d5becbdc53a573ee5ff14f0286b8b52a0a04
Delapouite <delapouite@gmail.com>
2018-05-04 10:33:16 +0200

    chore: remove obsolete samples

--- bin/parse-docx.js
+++ bin/parse-docx.js
@@ -1,14 +1,9 @@
 const { readFileSync } = require('fs')
-const { resolve } = require('path')
 
 const { parseArticle } = require('../server/lib/doc-parsers')
-
-const path =
-  process.argv[2] ||
-  resolve(__dirname, '../docs/samples/4/', '4A07 Paix négatives.docx')
 
 const main = async path => {
   const doc = await parseArticle(readFileSync(path))
   return JSON.stringify(doc, null, 2)
 }
-main(path).then(console.log, console.error)
+main(process.argv[2]).then(console.log, console.error)
--- bin/parse-lexicon.js
+++ bin/parse-lexicon.js
@@ -1,15 +1,10 @@
 const { readFileSync } = require('fs')
-const { resolve } = require('path')
 
 const { parseLexicon } = require('../server/lib/doc-parsers')
-
-const path =
-  process.argv[2] ||
-  resolve(__dirname, '../docs/samples/', 'lexique.docx')
 
 const main = async path => {
   const doc = await parseLexicon(readFileSync(path))
   return JSON.stringify(doc, null, 2)
 }
-main(path).then(console.log, console.error)
+main(process.argv[2]).then(console.log, console.error)
 

82a9b4906b28481b6425b63b6f33ca4ca3b472fd
Nicolas Chambrier <naholyr@gmail.com>
2018-10-04 16:43:42 +0200

    fix(html): properly sort topics and resources before passing root data to html generators (fix #162)

--- server/lib/site-builder.js
+++ server/lib/site-builder.js
@@ -80,11 +80,14 @@ const writeSitemapXml = async urls => {
 }
 
 const rebuildLangHTML = async lang => {
-  const options = { preview: false, lang }
-  const topics = populatePageUrl('topic', null, options)(await Topics.list())
-  topics.sort((t1, t2) => Number(t1.id) - Number(t2.id))
-  const resources = populatePageUrl(null, topics, options)(
-    await Resources.list(),
+  const topics = populatePageUrl('topic', null)(
+    await Topics.list({ sort: { id: 'desc' } }),
+  )
+  const resources = populatePageUrl(null, topics)(
+    await Resources.list({
+      query: Resources.DEFAULT_LIST_QUERY,
+      sort: { id: 'asc' },
+    }),
   )
   const publishedResources = resources.filter(
     ({ language, status, type }) =>

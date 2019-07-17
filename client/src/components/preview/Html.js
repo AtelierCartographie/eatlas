f5e61667c37bf0b8994f0dbcb725531230fe172e
Nicolas Chambrier <naholyr@gmail.com>
2019-07-17 10:41:38 +0200

    feat(a11y): all links provided in WYSIWYG open a new window

--- client/src/components/preview/Html.js
+++ client/src/components/preview/Html.js
@@ -3,7 +3,16 @@ const { stripTags } = require('../../universal-utils')
 const { ensureHTML } = require('./layout')
 const { injectIntl } = require('react-intl')
 
-const Html = injectIntl(props => {
+const Html = injectIntl((
+  props /*: {
+    noP?: boolean,
+    keepOriginalLinks?: boolean,
+    component?: string | Component,
+    children?: string | string[] | Component,
+    whitelist?: 'all' | string[],
+    intl?: any,
+  }*/,
+) => {
   // Default = only keep inline formatting
   const whitelist = props.whitelist || ['em', 'i', 'strong', 'b', 'a']
 
@@ -15,23 +24,21 @@ const Html = injectIntl(props => {
         : String(props.children)
     : null
 
-  const content =
+  let content =
     childContent === null
       ? null
       : whitelist === 'all'
         ? props.noP
-          ? handleExternalLinksTitle(childContent, props.intl)
-          : handleExternalLinksTitle(
-              ensureHTML(String(childContent)),
-              props.intl,
-            )
-        : handleExternalLinksTitle(
-            stripTags(String(childContent), whitelist),
-            props.intl,
-          )
+          ? childContent
+          : ensureHTML(String(childContent))
+        : stripTags(String(childContent), whitelist)
 
   if (content === null) {
     return null
+  }
+
+  if (props.intl && !props.keepOriginalLinks) {
+    content = handleLinks(content, props.intl)
   }
 
   const component = props.component || 'div'
@@ -42,33 +49,38 @@ const Html = injectIntl(props => {
   delete newProps.whitelist
   delete newProps.noP
   delete newProps.intl
+  delete newProps.keepOriginalLinks
   newProps.dangerouslySetInnerHTML = { __html: content }
 
   return h(component, newProps)
 })
 
+// #a11y: all links provided by WYSIWYG contents open a new window
+
 const RE_LINKS = /<a(.*?)>(.*?)<\/a>/gi
 const RE_TITLE = /\stitle=(?:(?:["'](.*?)["'])|(?:([^\s]+)))/
-const RE_TARGET_BLANK = /\starget=["']?_blank["']?/
+const RE_TARGET = /\starget=["']?([^\s]+)["']?/
 const RE_A = /<a/
 
-const handleExternalLinksTitle = (html, intl) => {
+const handleLinks = (html, intl) => {
   const newWindowMarker = intl.formatMessage(
     { id: 'fo.link-new-window-title' },
     { title: '' },
   )
   return html.replace(RE_LINKS, (match, attrs, text) => {
-    if (!attrs.match(RE_TARGET_BLANK)) {
-      return match
-    }
-    const matchTitle = ` ${attrs}`.match(RE_TITLE)
+    const sattrs = ` ${attrs}` // prepend with an initial space to include first attr
+    const matchTitle = sattrs.match(RE_TITLE)
     let title = matchTitle ? matchTitle[1] || matchTitle[2] : text
     if (title.indexOf(newWindowMarker) === -1) {
       title = intl.formatMessage({ id: 'fo.link-new-window-title' }, { title })
     }
-    return matchTitle
+    const replacedTitle = matchTitle
       ? match.replace(RE_TITLE, ` title="${title}"`)
       : match.replace(RE_A, `<a title="${title}"`)
+    const matchTarget = sattrs.match(RE_TARGET)
+    return matchTarget
+      ? replacedTitle.replace(RE_TARGET, ` target="_blank"`)
+      : replacedTitle.replace(RE_A, '<a target="_blank"')
   })
 }
 
